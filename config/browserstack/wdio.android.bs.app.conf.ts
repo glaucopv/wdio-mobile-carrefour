3332f55fd7b5765c353f10ab5920e02861589f61
glaucopv <glauco_02@hotmail.com>
2025-02-02 18:38:37 -0400

    Atualizando o config browserstack

--- config/browserstack/wdio.android.bs.app.conf.ts
+++ config/browserstack/wdio.android.bs.app.conf.ts
@@ -58,54 +58,54 @@ exports.config = {
     maxInstances: 1,
 >>>>>>> 4b9077b30418eb5fb8b13647ae4b1f00e8660284
 
-export const config: WebdriverIO.Config = {
-    ...baseConfig,
-    // ============
-    // Specs
-    // ============
-    specs: [
-        '../tests/specs/**/app*.spec.js',
-    ],
-    exclude: [
-        // Exclude this one because the test can only be executed on emulators/simulators
-        '../tests/specs/**/app.biometric.login.spec.js',
-    ],
+// export const config: WebdriverIO.Config = {
+//     ...baseConfig,
+//     // ============
+//     // Specs
+//     // ============
+//     specs: [
+//         '../tests/specs/**/app*.spec.js',
+//     ],
+//     exclude: [
+//         // Exclude this one because the test can only be executed on emulators/simulators
+//         '../tests/specs/**/app.biometric.login.spec.js',
+//     ],
 
-    // =============================
-    // Browserstack specific config
-    // =============================
-    // User configuration
-    user: process.env.BROWSERSTACK_USER || 'glaucovieira_A3xY1S',
-    key: process.env.BROWSERSTACK_ACCESS_KEY || 'WwBRTwwzUqashT3srBEx',
-    // Use browserstack service
-    services: ['browserstack'],
+//     // =============================
+//     // Browserstack specific config
+//     // =============================
+//     // User configuration
+//     user: process.env.BROWSERSTACK_USER || 'glaucovieira_A3xY1S',
+//     key: process.env.BROWSERSTACK_ACCESS_KEY || 'WwBRTwwzUqashT3srBEx',
+//     // Use browserstack service
+//     services: ['browserstack'],
 
-    // ============
-    // Capabilities
-    // ============
-    // For all capabilities please check
-    // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
-    capabilities: [
-        {
-            // Set URL of the application under test
-            'appium:app': process.env.BROWSERSTACK_APP_ID || 'bs://ad74f6ba445055d0d829daeb5cd648d74e80c342',
+//     // ============
+//     // Capabilities
+//     // ============
+//     // For all capabilities please check
+//     // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
+//     capabilities: [
+//         {
+//             // Set URL of the application under test
+//             'appium:app': process.env.BROWSERSTACK_APP_ID || 'bs://ad74f6ba445055d0d829daeb5cd648d74e80c342',
 
-            'bstack:options': {
-                // Set your BrowserStack config
-                debug: true,
+//             'bstack:options': {
+//                 // Set your BrowserStack config
+//                 debug: true,
 
-                // Specify device and os_version for testing
+//                 // Specify device and os_version for testing
 
-                deviceName: 'Samsung Galaxy S22 Ultra',
-                platformVersion: '12.0',
-                platformName: 'android',
+//                 deviceName: 'Samsung Galaxy S22 Ultra',
+//                 platformVersion: '12.0',
+//                 platformName: 'android',
 
 
-                // Set other BrowserStack capabilities
-                projectName: 'wdio-test-project',
-                buildName: 'android',
-                sessionName: 'wdio-test'
-            }
-        },
-    ] as WebdriverIO.Capabilities[]
+//                 // Set other BrowserStack capabilities
+//                 projectName: 'wdio-test-project',
+//                 buildName: 'android',
+//                 sessionName: 'wdio-test'
+//             }
+//         },
+//     ] as WebdriverIO.Capabilities[]
 };
